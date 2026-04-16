d4aa45bd09ad366552d6aecb7ba897dca8b8fa3f
diallomountaba <diallomountaba@gmail.com>
2026-04-16 19:48:03 +0000

    Add files via upload

---
+++ cv1.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+st.title("MOHAMED MOUNTABA DIALLO")
+st.divider()
+st.text("Geomaticien et Geographe,je suis motivé,déterminé et avec une bonne capacité physique et mentale.Je travaille toujours avec rigueur et munitie pour un résultat à la hauteur des attentes fixées.Grace à ma capacité d'adaptation et à mon ouverture d'esptit,j'assimile avec serieux et détermination l'objectif à atteindre.")
+
+st.subheader("Diplomes et Formations")
+st.markdown("""
+            
+ 1ere annee BTS geomatique au G15 Senegal-Inde 2026\n
+Baccalaureat Sciences Humaines et Sociales-Lycee Leopold Sedar Senghor depuis 2023
+* Bonne capacité d'analyse et d'interprétation des résultats expérimentaux
+* Bonne connaissances culturelle et littéraire
+* Bonne capacité à analyser les pensees et les affaires humaines,les critiquer et realiser des discours autour de ces sujets
+* Capacite d'agir et de prendre des decisions dans des affaires humaines et sociales importante
+* Capacite à comprendre les phenomenes humains dans leur dimension sociales,historiques,sectorielles,d'esprit et de loi
+* Bonne maitrise des outils informatiques et des logiciels
+""")
+
+st.divider()
+st.subheader("Experience personnelle")
+st.text("Pratique continue du sport en parellèle d'un parcours academique.\n" \
+"Cette double implication m'a permis de develloper une forte discipline ,une excelente gestion du temps et une grande capacite à travaller sous pression. ")
+st.markdown("""
+* Rigueur et sens de l'organisation
+* Esprit d'équipe et leadership
+* Perséverance et dépassement de soi 
+* Capacite a concilier plusieurs responsabilités""")
+st.text("Cette experience m'a permis de forger un profil équilibrer,dynamique et orienté vers la performance.")
+
+st.divider()
+st.subheader("Sport")
+st.markdown("""
+* Maitre de Dojo            
+* Ceinture Noire 2e Duan-Kungfu Wushu 2024
+* Ceinture Noire 1ere Duan-kungfu Wushu 2022
+* Ceinture Rouge superieure-Kungfu Wushu 2020
+* Plusieurs fois Champion du Sénégal
+            """)
+
+with st.sidebar:
+    st.markdown(""" Contacts
+* Citee Lycee-Joal
+* +221765758529
+* diallomohamed922@gmail.com
+                """)
+    st.markdown("""Langues
+* Français
+* Anglais
+                 """)
+    st.divider()
+    st.subheader("Centre D'intérêt")
+    st.write("Sport")
+    st.write("Lecture")
+    st.write("Cuisine")
+    st.divider()
+    st.subheader("Competences")
+    st.write("Bonne maitrise des logiciels d'architecture(Autocad,SketchUp)")
+    st.write("Bonne maitrise des logiciels cartographique(Arcgis,QGiS)")
+    st.write("Capacite d'adaptation")
+    st.write("Ouverture d'esprit")
+    st.write("Methodique")
+    st.write("Proactif")
+    st.write("Social")
+    
