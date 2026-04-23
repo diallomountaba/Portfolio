106acb643275cfc34d0c9a229ce0b5ebfe43a9bb
diallomountaba <diallomountaba@gmail.com>
2026-04-23 22:18:26 +0000

    Update cv1.py

--- cv1.py
+++ cv1.py
@@ -5,19 +5,18 @@ st.text("Geomaticien et Geographe,je suis motivé,déterminé et avec une bonne 
 st.subheader("Parcours Académique")
 st.markdown("""
             
- 1ere annee BTS geomatique au G15 Senegal-Inde 2026\n
-Baccalaureat Sciences Humaines et Sociales-Lycee Leopold Sedar Senghor depuis 2023
+* Brevet de Téchnicien Supérieur en Géomatique 2026
+* Baccalaureat Sciences Humaines et Sociales 2023
 * Bonne capacité d'analyse et d'interprétation des résultats expérimentaux
 * Bonne connaissances culturelle et littéraire
 * Bonne capacité à analyser les pensees et les affaires humaines,les critiquer et realiser des discours autour de ces sujets
 * Capacite d'agir et de prendre des decisions dans des affaires humaines et sociales importante
 * Capacite à comprendre les phenomenes humains dans leur dimension sociales,historiques,sectorielles,d'esprit et de loi
-* Bonne maitrise des outils informatiques et des logiciels
 """)
 
 st.divider()
-st.subheader("Experience personnelle")
-st.text("Pratique continue du sport en parellèle d'un parcours academique.\n" \
+st.subheader("Expériences personnelle")
+st.text("Pratique continue du sport pendant 13ans en parellèle d'un parcours academique.\n" \
 "Cette double implication m'a permis de develloper une forte discipline ,une excelente gestion du temps et une grande capacite à travaller sous pression. ")
 st.markdown("""
 * Rigueur et sens de l'organisation
@@ -40,11 +39,9 @@ with st.sidebar:
             st.texte("MOHAMED MOUNTABA DIALLO")
     st.markdown(""" Contacts
 * Citee Lycee-Joal
-* +221765758529
 * diallomohamed922@gmail.com
                 """)
     st.markdown("""Langues
-* Français
 * Anglais
                  """)
     st.divider()
@@ -53,7 +50,7 @@ with st.sidebar:
     st.write("Lecture")
     st.write("Cuisine")
     st.divider()
-    st.subheader("Competences")
+    st.subheader("Compétences")
     st.write("Bonne maitrise des logiciels d'architecture(Autocad,SketchUp)")
     st.write("Bonne maitrise des logiciels cartographique(Arcgis,QGiS)")
     st.write("Capacite d'adaptation")
