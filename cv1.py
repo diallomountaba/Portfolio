22f30ef22f2348fd255ec860fc1e94f40ad57504
diallomountaba <diallomountaba@gmail.com>
2026-04-24 13:27:45 +0000

    Update cv1.py

--- cv1.py
+++ cv1.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.info("PROFIL")
 st.text("Téchnicien supérieur en Géomatique,motivé,déterminé et avec une bonne capacité physique et mentale.Je travaille toujours avec rigueur et munitie pour un résultat à la hauteur des attentes fixées.Grace à ma capacité d'adaptation et à mon ouverture d'esptit,j'assimile avec serieux et détermination l'objectif à atteindre.")
 
-st.subheader("PARCOURS ACADEMIQUE")
+st.info("PARCOURS ACADEMIQUE")
 st.markdown("""
 * Licence en Géographie 2026          
 * Brevet de Téchnicien Supérieur en Géomatique 2026
@@ -14,11 +14,6 @@ st.divider()
 st.info("EXPERIENCE PERSONNELLE")
 st.text("Pratique continue du sport pendant 13ans en parellèle d'un parcours academique.\n" \
 "Cette double implication m'a permis de develloper une forte discipline ,une excelente gestion du temps et une grande capacite à travaller sous pression. ")
-st.markdown("""
-* Rigueur et sens de l'organisation
-* Esprit d'équipe et leadership
-* Perséverance et dépassement de soi 
-* Capacite a concilier plusieurs responsabilités""")
 st.text("Cette experience m'a permis de forger un profil équilibrer,dynamique et orienté vers la performance.")
 
 st.divider()
@@ -33,25 +28,31 @@ st.markdown("""
 
 with st.sidebar:
     st.subheader("MOHAMED MOUNTABA DIALLO")
-    st.markdown(""" 📞Contacts
+    st.markdown("""
 * 📍Citee Lycee-Joal
 * ✉️diallomohamed922@gmail.com
                 """)
-    st.success("""LANGUE
+    st.success(""" LANGUE
 * Anglais
                  """)
-    st.divider()
-    st.success("CENTRE D'INTERET")
-    st.write("Sport")
-    st.write("Lecture")
-    st.write("Cuisine")
+            
     st.divider()
     st.success("COMPETENCES")
+with col1:
     st.write("Bonne maitrise des logiciels d'architecture(Autocad,SketchUp)")
     st.write("Bonne maitrise des logiciels cartographique(Arcgis,QGiS)")
     st.write("Capacite d'adaptation")
     st.write("Ouverture d'esprit")
+with col2:
     st.write("Methodique")
     st.write("Proactif")
     st.write("Social")
+    st.write("Bonne capacité d'analyse et d'interprétation des résultats expérimentaux")
+    st.write("Bonne connaissances culturelle et littéraire")
+    st.write("Bonne capacité à analyser les pensees et les affaires humaines,les critiquer et realiser des discours autour de ces sujets")
     
+* Bonne capacité d'analyse et d'interprétation des résultats expérimentaux
+* Bonne connaissances culturelle et littéraire
+* Bonne capacité à analyser les pensees et les affaires humaines,les critiquer et realiser des discours autour de ces sujets
+* Capacite d'agir et de prendre des decisions dans des affaires humaines et sociales importante
+* Capacite à comprendre les phenomenes humains dans leur dimension sociales,historiques,sectorielles,d'esprit et de loi
